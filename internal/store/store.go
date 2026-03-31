50ccef8e5ed8b19d9e67a4b34f748b001b4b3b08
greg-beach <gbeach22@gmail.com>
2026-03-31 06:14:23 -0400

    update the store.Lookup, function was logging and returning an error. Now only returns an error that can be unwrapped in upper function

--- internal/store/store.go
+++ internal/store/store.go
@@ -109,8 +109,7 @@ func (s *Store) Lookup(_ context.Context, short string) (string, error) {
 		return "", ErrNotFound
 	}
 	if err != nil {
-		s.logger.Error(fmt.Sprintf("failed to read %s: %v\n", shortcodeFilepath, err))
-		return "", err
+		return "", fmt.Errorf("read %s: %w", shortcodeFilepath, err)
 	}
 	return string(data), nil
 }
