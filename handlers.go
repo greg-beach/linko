7472960687b2fb4682cf7522b54112651f929025
greg-beach <gbeach22@gmail.com>
2026-04-19 07:08:52 -0400

    added some logic to redact some sensitive keys from the logging attributes

--- handlers.go
+++ handlers.go
@@ -62,7 +62,7 @@ func (s *server) handlerShortenLink(w http.ResponseWriter, r *http.Request) {
 	}
 	s.logger.Info("Successfully generated short code",
 		slog.String("code", shortCode),
-		slog.String("URL", longURL),
+		slog.String("long_url", longURL),
 	)
 	w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(http.StatusCreated)
